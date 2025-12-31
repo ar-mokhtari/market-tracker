e5e4e71637b7bf3308b3ff2cd14cb4aa0c60e3db
AlirezaMG@HQX <ar.mokhtari.g@gmail.com>
2025-12-31 13:58:26 +0330

    fix(config.go):  default of cfg.FetchInterval is 5 min now

--- config/config.go
+++ config/config.go
@@ -57,7 +57,7 @@ func Init() *Config {
 		log.Fatal("Critical: DB_DSN is not set in environment")
 	}
 
-	cfg.FetchInterval = getEnvAsInt("FETCH_INTERVAL", 1)
+	cfg.FetchInterval = getEnvAsInt("FETCH_INTERVAL", 5)
 
 	return cfg
 }
